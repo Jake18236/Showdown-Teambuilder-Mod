24235a59ffc7791f179f0b5c865d22f000f324f2
Jake18236 <144159145+Jake18236@users.noreply.github.com>
2026-09-19 18:51:36 -0700

    Add initial user script for Pokémon Showdown Teambuilder

---
+++ showdown-teambuilder.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Pokémon Showdown Teambuilder QOL
+// @namespace    https://github.com/Jake18236/showdown-teambuilder-mod
+// @version      1.2
+// @description  Makes the Showdown Teambuilder better for some OMs
+// @match        https://play.pokemonshowdown.com/*
+// @grant        none
+// @run-at       document-start
+// @updateURL    https://raw.githubusercontent.com/Jake18236/showdown-teambuilder-mod/main/showdown-teambuilder.user.js
+// @downloadURL  https://raw.githubusercontent.com/Jake18236/showdown-teambuilder-mod/main/showdown-teambuilder.user.js
+// ==/UserScript==
